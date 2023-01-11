32f2ea952bc8b95c1102f42e91e2471310dde42c
Declan_Chiu <1282117217@qq.com>
2023-01-12 00:33:41 +0800

    feat: ✨ 新增 isString

--- packages/guitar-utils/src/index.ts
+++ packages/guitar-utils/src/index.ts
@@ -1,3 +1,13 @@
+const toString = Object.prototype.toString;
+
+export function is(val: unknown, type: string) {
+  return toString.call(val) === `[object ${type}]`;
+}
+
 export const isArray = (val: any) => {
   return Array.isArray(val);
+}
+
+export const isString = (val: unknown): val is string => {
+  return is(val, 'String');
 }
