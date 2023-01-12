3ded7a1fecc4f3a6e8d7f7e6e210d39e8bd79a02
Declan_Chiu <1282117217@qq.com>
2023-01-13 00:37:43 +0800

    test: 🧪 新增 isBoolean 的单测用例

--- apps/vite-test-demo/test/is.test.ts
+++ apps/vite-test-demo/test/is.test.ts
@@ -1,6 +1,6 @@
-import { assert, expect, test } from 'vitest';
+import { expect, test } from 'vitest';
 
-import { isArray, isFunction } from '@guitar/utils';
+import { isArray, isFunction, isBoolean } from '@guitar/utils';
 
 test("isArray", () => {
   expect(isArray([])).toBe(true);
@@ -9,4 +9,9 @@ test("isArray", () => {
 test("isFunction", () => {
   const test = () => {};
   expect(isFunction(test)).toBe(true);
+})
+
+test("isBoolean", () => {
+  const isShow = true;
+  expect(isBoolean(isShow)).toBe(true);
 })
