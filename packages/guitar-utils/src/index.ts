b9b4fc8e3b700782bf5875a7d55b5d446ea5f85d
Declan_Chiu <1282117217@qq.com>
2023-01-14 04:45:12 +0800

    feat: ✨ 新增 isUnDef 方法

--- packages/guitar-utils/src/index.ts
+++ packages/guitar-utils/src/index.ts
@@ -27,3 +27,7 @@ export const isDate = (val: unknown): val is Date => {
 export function isDef<T = unknown>(val?: T): val is T {
   return typeof val !== 'undefined';
 }
+
+export function isUnDef<T = unknown>(val?: T): val is T {
+  return !isDef(val);
+}
