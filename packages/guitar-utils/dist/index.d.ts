85526e1fcbf0fcea8b6db0bb2ddd15b474a042ec
Declan_Chiu <1282117217@qq.com>
2023-01-13 23:55:01 +0800

    fix: 🐛 修复 declan-utils 的多余文件

--- packages/guitar-utils/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare function is(val: unknown, type: string): boolean;
-declare const isArray: (val: any) => boolean;
-declare const isString: (val: unknown) => val is string;
-
-export { is, isArray, isString };
